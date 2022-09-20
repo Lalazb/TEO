5fa5f83e595173685e4970d50718b998eb0ef075
a00824999 <112212623+a00824999@users.noreply.github.com>
2022-09-19 20:17:11 -0600

    Add files via upload

---
+++ Scripts/Water.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.GetComponent<PController>().isSwimming = true;
+            other.transform.GetComponent<Rigidbody>().useGravity = true;
+        }
+        /*if (other.CompareTag("Player"))
+        {
+            PController pcontroller = other.GetComponent<PController>();
+            pcontroller.isSwimming = true; //REFERENC�A AL BOOL EN MOVEMENT PARA TRUE O FALSE
+            other.GetComponent<Swimming>().enabled = true;
+            other.GetComponent<PController>().enabled = false;
+        }*/
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.GetComponent<PController>().isSwimming = false;
+            other.transform.GetComponent<Rigidbody>().useGravity = false;
+        }
+        /*if (other.CompareTag("Player"))
+        {
+            PController pcontroller = other.GetComponent<PController>();
+            pcontroller.isSwimming = false; //REFERENC�A AL BOOL EN MOVEMENT PARA TRUE O FALSE
+            other.GetComponent<Swimming>().enabled = false;
+            other.GetComponent<PController>().enabled = true;
+        }*/
+    }
+}
