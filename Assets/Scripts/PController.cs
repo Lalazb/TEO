0d1d9e948f57b5098916ad661124c431b1c0e849
Chris L <chris.rey.lop.g@gmail.com>
2022-09-19 19:27:09 -0500

    Water Update

--- Assets/Scripts/PController.cs
+++ Assets/Scripts/PController.cs
@@ -8,9 +8,11 @@ public class PController : MonoBehaviour
     //public variables
     public CharacterController controller;
     public float speed = 8;
+    public float swimSpeed = 2;
     public float dash = 100;
     public float jumpForce = 10;
     public float gravity = -20;
+    public float waterGravity = -0.01f;
     public Transform groundCheck;
     public LayerMask groundLayer;
     public bool isSwimming = false;
@@ -19,6 +21,7 @@ public class PController : MonoBehaviour
     //private variables
     private bool isGrounded=true;
     private Vector3 direction;
+    private Rigidbody body;
     // Update is called once per frame
 
     public void FixSpriteHorizontalOrientation()
@@ -33,8 +36,12 @@ public class PController : MonoBehaviour
         }
     }
 
-    //probar con ray
+    private void Start()
+    {
+        this.body = this.GetComponent<Rigidbody>();
+    }
 
+    //probar con ray
     void FixedUpdate()
     {
         this.FixSpriteHorizontalOrientation();
@@ -42,35 +49,57 @@ public class PController : MonoBehaviour
         float hInput = Input.GetAxis("Horizontal");
         
         isGrounded = Physics.CheckSphere(groundCheck.position, 0.2f, groundLayer);
-      
-      //revisar lineas 48-65 iniciando por jump
-        if (isGrounded && Input.GetButtonDown("Jump"))
+
+        if(isSwimming)
         {
             direction.y = -1;
-            ableToMakeDoubleJump = true;
-            direction.y = jumpForce;
-            //Debug.Log("Jump");
-        }
-        else
-        {   
-            direction.y += gravity * Time.fixedDeltaTime;
-            if (ableToMakeDoubleJump & Input.GetButtonDown("Jump"))
-            {
-                direction.y = jumpForce;
-                ableToMakeDoubleJump = false;
-            }
-        }
 
-        //Dash
-        if(Input.GetKeyDown(KeyCode.Tab))
-        {
-            direction.x = hInput * dash;
+            if (Input.GetButtonDown("Jump"))
+            {                
+                direction.y = jumpForce;
+                Debug.Log("Swim");
+            }
+
+            direction.y += waterGravity * Time.fixedDeltaTime;
+            Debug.Log("Water Gravity");
+
+            direction.x = hInput * swimSpeed;
             controller.Move(direction * Time.fixedDeltaTime);
         }
         else
         {
-            direction.x = hInput * speed;
-            controller.Move(direction * Time.fixedDeltaTime);
+            //revisar lineas 48-65 iniciando por jump
+            if (isGrounded)
+            {
+                direction.y = -1;
+                if(Input.GetButtonDown("Jump"))
+                {
+                    ableToMakeDoubleJump = true;
+                    direction.y = jumpForce;
+                    //Debug.Log("Jump");
+                }
+            }
+            else
+            {   
+                direction.y += gravity * Time.fixedDeltaTime;
+                Debug.Log("Gravity");
+                if (ableToMakeDoubleJump & Input.GetButtonDown("Jump"))
+                {
+                    direction.y = jumpForce;
+                    ableToMakeDoubleJump = false;
+                }
+            }
+            //Dash
+            if(Input.GetKeyDown(KeyCode.Tab))
+            {
+                direction.x = hInput * dash;
+                controller.Move(direction * Time.fixedDeltaTime);
+            }
+            else
+            {
+                direction.x = hInput * speed;
+                controller.Move(direction * Time.fixedDeltaTime);
+            }
         }
     }
 }
--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -9,21 +9,29 @@ public class Water : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            other.transform.GetComponent<PController>().isSwimming = true;
+        }
+        /*if (other.CompareTag("Player"))
+        {
             PController pcontroller = other.GetComponent<PController>();
             pcontroller.isSwimming = true; //REFERENC�A AL BOOL EN MOVEMENT PARA TRUE O FALSE
             other.GetComponent<Swimming>().enabled = true;
             other.GetComponent<PController>().enabled = false;
-        }
+        }*/
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            other.transform.GetComponent<PController>().isSwimming = false;
+        }
+        /*if (other.CompareTag("Player"))
+        {
             PController pcontroller = other.GetComponent<PController>();
             pcontroller.isSwimming = false; //REFERENC�A AL BOOL EN MOVEMENT PARA TRUE O FALSE
             other.GetComponent<Swimming>().enabled = false;
             other.GetComponent<PController>().enabled = true;
-        }
+        }*/
     }
 }
