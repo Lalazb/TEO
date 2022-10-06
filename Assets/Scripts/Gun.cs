0b55ad76afc2a03cdd940ce074262f55742b1c86
a00824999 <112212623+a00824999@users.noreply.github.com>
2022-10-06 14:18:27 -0600

    Water gun-Daniel

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+    //
+{
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private Transform firepoint;
+
+    [SerializeField] private GameObject chargedProjectile;
+    [SerializeField] private float chargeSpeed;
+    [SerializeField] private float chargeTime;
+    private bool isCharging;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F) && chargeTime < 2) 
+        {
+            isCharging = true;
+            if(isCharging == true)
+            {
+                chargeTime += Time.deltaTime * chargeSpeed;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Instantiate(projectile, firepoint.position, firepoint.rotation);
+            chargeTime = 0;
+        } else if(Input.GetKeyUp(KeyCode.F) && chargeTime >= 2)
+        {
+            ReleaseCharge();
+        }
+    }
+
+
+    void ReleaseCharge()
+    {
+        Instantiate(chargedProjectile, firepoint.position, firepoint.rotation);
+        isCharging = false;
+        chargeTime = 0;
+    }
+}
