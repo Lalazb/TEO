7e1db9fd64df2287301b4db2094be174d22c71d0
a00824999 <112212623+a00824999@users.noreply.github.com>
2022-11-03 23:40:43 -0600

    Animator

--- Assets/Scripts/PController.cs
+++ Assets/Scripts/PController.cs
@@ -11,7 +11,6 @@ public class PController : MonoBehaviour
     public float NT = 0f;
     public float resta = 1;
     public bool moving;
-    //public float waterGravity;
     public bool hability;
     public bool isGrounded;
     public bool isSwiming;
@@ -19,14 +18,28 @@ public class PController : MonoBehaviour
     public Transform model;
     public Transform groundCheck;
     public LayerMask groundLayer;
-    public LayerMask waterMask;
 
     private Vector3 direction;
+    Animator animator;
+
+    enum TeoStates
+    {
+        Idle,
+        Walk,
+        Jump,
+        Caida,
+        Landing,
+        WaterStream
+    };
+    TeoStates state;
+
 
     // Start is called before the first frame update
     void Start()
     {
         moving = true;
+        animator = GetComponent<Animator>();
+        ChangeState(TeoStates.Idle);
     }
 
     // Update is called once per frame
@@ -58,7 +71,7 @@ public class PController : MonoBehaviour
             //Flip
             if (hInput != 0)
             {
-                Quaternion newRotation = Quaternion.LookRotation(new Vector3(0, 0, hInput));
+                Quaternion newRotation = Quaternion.LookRotation(new Vector3(hInput, 0, 0));
                 model.rotation = newRotation;
             }
 
@@ -103,34 +116,34 @@ public class PController : MonoBehaviour
             }
         }
 
-        //Swiming
-
-        isSwiming = Physics.CheckSphere(groundCheck.position, 0.2f, waterMask);
-
-        if (isSwiming)
-        {
-            gravity = -4f;
-            speed = 8f;
-            jumpForce = 6f;
-            if (Input.GetButtonUp("Jump"))
-            {
-                direction.y = jumpForce;
-                // controller.Move(Vector3.right);
-            }
-
-        }
-        else
-        {
-            speed = 8f;
-            gravity = -20f;
-            jumpForce = 10f;
-        }
-
         //Prueba
         if (TeoState.vidas <= 0 || TeoState.resp == 1)
         {
             StartCoroutine(PausaRes());
         }
+
+        switch(state)
+        {
+            case TeoStates.Idle:
+                Idle();
+                break;
+            case TeoStates.Walk:
+                Walk();
+                break;
+            case TeoStates.Jump:
+                Jump();
+                break;
+            case TeoStates.Caida:
+                Caida();
+                break;
+            case TeoStates.Landing:
+                Landing();
+                break;
+            case TeoStates.WaterStream:
+                WaterStream();
+                break;
+        }
+
     }
     private void CheckRoof()
     {
@@ -149,4 +162,64 @@ public class PController : MonoBehaviour
         NT = 0;
         moving = true;
     }
+    
+    void ChangeState(TeoStates newState)
+    {
+        switch (newState)
+        {
+            case TeoStates.Idle:
+                animator.SetBool("isGrounded", true);
+                animator.SetBool("isMoving", false);
+                break;
+            case TeoStates.Walk:
+                animator.SetBool("isGrounded", true);
+                animator.SetBool("isMoving", true);
+                break;
+            case TeoStates.Jump:
+                animator.SetBool("isGrounded", false);
+                break;
+            case TeoStates.Caida:
+                
+                break;
+            case TeoStates.Landing:
+                animator.SetBool("isGrounded", true);
+                break;
+            case TeoStates.WaterStream:
+                animator.ResetTrigger("waterBomb");
+                break;
+        }
+        state = newState;
+    }
+
+
+    void Idle()
+    {
+        ChangeState(TeoStates.Idle);
+    }
+
+    void Walk()
+    {
+        ChangeState(TeoStates.Walk);
+    }
+
+    void Jump()
+    {
+
+    }
+
+    void Caida()
+    {
+
+    }
+
+    void Landing()
+    {
+
+    }
+
+    void WaterStream()
+    {
+
+    }
+
 }
