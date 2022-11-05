144f1516030ec23020272b467a849e82e37eb945
a00824999 <112212623+a00824999@users.noreply.github.com>
2022-11-05 15:42:14 -0600

    nivel

--- Assets/Scripts/PController.cs
+++ Assets/Scripts/PController.cs
@@ -6,11 +6,12 @@ public class PController : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 8f;
-    public float jumpForce = 10f;
-    public float gravity = -20f;
+    public float jumpForce = 5f;
+    public float gravity = -10f;
     public float NT = 0f;
     public float resta = 1;
     public bool moving;
+    //public float waterGravity;
     public bool hability;
     public bool isGrounded;
     public bool isSwiming;
@@ -18,28 +19,14 @@ public class PController : MonoBehaviour
     public Transform model;
     public Transform groundCheck;
     public LayerMask groundLayer;
+    public LayerMask waterMask;
 
     private Vector3 direction;
-    Animator animator;
-
-    enum TeoStates
-    {
-        Idle,
-        Walk,
-        Jump,
-        Caida,
-        Landing,
-        WaterStream
-    };
-    TeoStates state;
-
 
     // Start is called before the first frame update
     void Start()
     {
         moving = true;
-        animator = GetComponent<Animator>();
-        ChangeState(TeoStates.Idle);
     }
 
     // Update is called once per frame
@@ -53,7 +40,7 @@ public class PController : MonoBehaviour
         }
         else
         {
-            speed = 10;
+            speed = 8;
         }
         if (TeoState.nslow == 1)
         {
@@ -71,7 +58,7 @@ public class PController : MonoBehaviour
             //Flip
             if (hInput != 0)
             {
-                Quaternion newRotation = Quaternion.LookRotation(new Vector3(hInput, 0, 0));
+                Quaternion newRotation = Quaternion.LookRotation(new Vector3(0, 0, hInput));
                 model.rotation = newRotation;
             }
 
@@ -116,34 +103,34 @@ public class PController : MonoBehaviour
             }
         }
 
+        //Swiming
+
+        isSwiming = Physics.CheckSphere(groundCheck.position, 0.2f, waterMask);
+
+        if (isSwiming)
+        {
+            gravity = -4f;
+            speed = 8f;
+            jumpForce = 6f;
+            if (Input.GetButtonUp("Jump"))
+            {
+                direction.y = jumpForce;
+                // controller.Move(Vector3.right);
+            }
+
+        }
+        else
+        {
+            speed = 8f;
+            gravity = -20f;
+            jumpForce = 10f;
+        }
+
         //Prueba
         if (TeoState.vidas <= 0 || TeoState.resp == 1)
         {
             StartCoroutine(PausaRes());
         }
-
-        switch(state)
-        {
-            case TeoStates.Idle:
-                Idle();
-                break;
-            case TeoStates.Walk:
-                Walk();
-                break;
-            case TeoStates.Jump:
-                Jump();
-                break;
-            case TeoStates.Caida:
-                Caida();
-                break;
-            case TeoStates.Landing:
-                Landing();
-                break;
-            case TeoStates.WaterStream:
-                WaterStream();
-                break;
-        }
-
     }
     private void CheckRoof()
     {
@@ -162,64 +149,4 @@ public class PController : MonoBehaviour
         NT = 0;
         moving = true;
     }
-    
-    void ChangeState(TeoStates newState)
-    {
-        switch (newState)
-        {
-            case TeoStates.Idle:
-                animator.SetBool("isGrounded", true);
-                animator.SetBool("isMoving", false);
-                break;
-            case TeoStates.Walk:
-                animator.SetBool("isGrounded", true);
-                animator.SetBool("isMoving", true);
-                break;
-            case TeoStates.Jump:
-                animator.SetBool("isGrounded", false);
-                break;
-            case TeoStates.Caida:
-                
-                break;
-            case TeoStates.Landing:
-                animator.SetBool("isGrounded", true);
-                break;
-            case TeoStates.WaterStream:
-                animator.ResetTrigger("waterBomb");
-                break;
-        }
-        state = newState;
-    }
-
-
-    void Idle()
-    {
-        ChangeState(TeoStates.Idle);
-    }
-
-    void Walk()
-    {
-        ChangeState(TeoStates.Walk);
-    }
-
-    void Jump()
-    {
-
-    }
-
-    void Caida()
-    {
-
-    }
-
-    void Landing()
-    {
-
-    }
-
-    void WaterStream()
-    {
-
-    }
-
 }
